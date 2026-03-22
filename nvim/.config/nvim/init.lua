155c33c3e189b940e4286a175bb378d8a397865d
Jacob Meyer <jakem3350@gmail.com>
2026-03-21 22:09:01 -0400

    formatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,14 +3,14 @@
 -- install lazy if not already present
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -18,8 +18,5 @@ vim.g.mapleader = " "
 
 local opts = {}
 
--- ALL added plugins must go after lazy
 require("lazy").setup("plugins")
 require("vim-settings")
-
-
--- nvim/.config/nvim/lua/plugins/codediff.lua
+++ nvim/.config/nvim/lua/plugins/codediff.lua
@@ -1,24 +1,24 @@
 return {
-  {
-    "esmuellert/codediff.nvim",
-    config = function()
-      require('codediff').setup({})
-    end
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require('gitsigns').setup({
-        signs = {
-          add          = { text = '┃' },
-          change       = { text = '┃' },
-          delete       = { text = '_' },
-          topdelete    = { text = '‾' },
-          changedelete = { text = '~' },
-          untracked    = { text = '┆' },
-        },
-        update_debounce = 100
-      })
-    end
-  }
+	{
+		"esmuellert/codediff.nvim",
+		config = function()
+			require("codediff").setup({})
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				update_debounce = 100,
+			})
+		end,
+	},
 }
