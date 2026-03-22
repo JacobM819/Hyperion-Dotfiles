a68cb9f5d664a414a66841296fdd16d3747e956b
Jacob Meyer <jakem3350@gmail.com>
2026-03-22 13:29:59 -0400

    Added alpha-nvim and updated telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
 		config = function()
 			-- telescope
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 			-- global grep
 			vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
 		end,
