6f9e77e5974ee6620b118329bb40278c5cfad26f
Jacob Meyer <jakem3350@gmail.com>
2026-03-23 13:33:59 -0400

    some things

--- nvim/.config/nvim/lua/vim-settings.lua
+++ nvim/.config/nvim/lua/vim-settings.lua
@@ -4,9 +4,13 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
 vim.opt.termguicolors = true
+
+-- Line number settings
 vim.opt.number = true
 vim.opt.relativenumber = true
+--vim.api.nvim_set_hl(0, "LineNr", { fg = "#cdd6f4", bold = true })
 
+-- Tab settings
 vim.opt.shiftwidth = 3
 vim.opt.tabstop = 3
 vim.opt.expandtab = true
